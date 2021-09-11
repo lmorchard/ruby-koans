b8255fd51c6c62d8911490f42fcb7df91c17b0ab
Les Orchard <me@lmorchard.com>
2021-09-11 15:09:42 -0700

    triangle puttering and progress on exceptions

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # https://stackoverflow.com/questions/4742692/a-more-elegant-solution-to-ruby-koans-triangle-rb
+  case [a,b,c].uniq.size
+  when 1 then :equilateral
+  when 2 then :isosceles
+  else        :scalene
+  end
+
+  # my original answer
+  #if a == b and b == c
+  #  :equilateral
+  #elsif a == b or b == c or c == a
+  #  :isosceles
+  #else
+  #  :scalene
+  #end
 end
 
 # Error class used in part 2.  No need to change this code.
